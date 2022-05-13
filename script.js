ab5db6c7c44f6599e45e8fe684dd029257d8c848
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2022-05-13 21:55:58 +0200

    Starting ToDo CRUD App

--- script.js
+++ script.js
@@ -1,54 +1,63 @@
-let form = document.querySelector("#form");
 
-let input = form.querySelector("#input");
-let msg = form.querySelector("#msg");
 
-let posts = document.querySelector("#posts");
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
+
+
+
+
+
+// logic for dummy app
+// let form = document.querySelector("#form");
+
+// let input = form.querySelector("#input");
+// let msg = form.querySelector("#msg");
+
+// let posts = document.querySelector("#posts");
+
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault();
     
-    formValidation();
-});
+//     formValidation();
+// });
 
-let formValidation = () => {
-    if (input.value === "") {
-        msg.innerHTML = "Post cannot be blank";
-        console.log("failure");
-    } else {
-        console.log("successs");
-        msg.innerHTML = "";
-        acceptData();
-    }
-}
+// let formValidation = () => {
+//     if (input.value === "") {
+//         msg.innerHTML = "Post cannot be blank";
+//         console.log("failure");
+//     } else {
+//         console.log("successs");
+//         msg.innerHTML = "";
+//         acceptData();
+//     }
+// }
 
-let data = {};
+// let data = {};
 
-let acceptData = () => {
-    data['text'] = input.value;
+// let acceptData = () => {
+//     data['text'] = input.value;
     
-    createPost();
-}
+//     createPost();
+// }
 
-let createPost = () => {
-    posts.innerHTML += `
-        <div>
-            <p>${data.text}</p>
-            <span class="options">
-                <i onClick="editPost(this)" class="fas fa-edit"></i>
-                <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
-            </span>
-        </div>
-    `;
+// let createPost = () => {
+//     posts.innerHTML += `
+//         <div>
+//             <p>${data.text}</p>
+//             <span class="options">
+//                 <i onClick="editPost(this)" class="fas fa-edit"></i>
+//                 <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
+//             </span>
+//         </div>
+//     `;
 
-    form.reset();
-}
+//     form.reset();
+// }
 
-let deletePost = (e) => {
-    e.parentElement.parentElement.remove();
-}
+// let deletePost = (e) => {
+//     e.parentElement.parentElement.remove();
+// }
 
-let editPost = (e) => {
-    input.value = e.parentElement.previousElementSibling.innerHTML;
-    e.parentElement.parentElement.remove();
-}
+// let editPost = (e) => {
+//     input.value = e.parentElement.previousElementSibling.innerHTML;
+//     e.parentElement.parentElement.remove();
+// }
